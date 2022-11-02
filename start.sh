97b9b52c4726298a3b1b01d4ef833eea6df7965b
yazhuyang <85384449+yazhuyang@users.noreply.github.com>
2022-11-02 10:47:21 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=de157513-c8b7-4c0c-84bd-bb74d1ea179d
+AUUID=ab8c1f8d-5cd8-44f5-a019-a7018139381d
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/yazhuyang/kinto/main/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/yazhuyang/kinto/main/etc/xray.json
